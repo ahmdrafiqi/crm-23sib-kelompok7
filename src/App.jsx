d5049c4f6f07f264821d3a41b6c3be0f1d808cf9
Ahmad Rafiqi <rafiqi23si@mahasiswa.pcr.ac.id>
2025-05-21 16:48:20 +0700

    Sudah di perbaiki

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
 // import { Route } from 'lucide-react'
 // import './App.css'
-import { Routes, Route } from 'react-router-dom'
-import MainLayout from './components/MainLayout'
-import Dashboard from './pages/Dashboard'
+import { Routes, Route } from "react-router-dom";
+import MainLayout from "./components/MainLayout";
+import Dashboard from "./pages/Dashboard";
+import Penjualan from "./pages/Penjualan";
+import Pelanggan from "./pages/Pelanggan";
+
+
 
 
 function App() {
-
   return (
     <Routes>
       <Route element={<MainLayout />}>
-        <Route path='/' element={ <Dashboard />} />
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/pelanggan" element={<Pelanggan />} />
+        <Route path="/penjualan" element={<Penjualan />} />
       </Route>
     </Routes>
-  )
+  );
 }
 
-export default App
+export default App;
